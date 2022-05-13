56c728352aa37a5a588937224abf2682fdaea881
cyrus311 <shadow15mr@hotmail.com>
2022-05-13 23:46:04 +0300

    mock example added

--- src/data.test.js
+++ src/data.test.js
@@ -3,7 +3,8 @@ import { generateReportData } from "./data";
 
 describe("generateReportData()", () => {
   it("should execute logFn if provided", () => {
-    const logger = vi.fn();
+    const logger = vi.fn(() => {});
+    // logger.mockImplementationOnce(() => {});
 
     generateReportData(logger);
 
